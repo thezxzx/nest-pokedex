231e50566b4b8e6b2ff5adc8119380118466dd52
thezxzx <themasterzxzx@gmail.com>
2023-03-11 16:34:05 -0700

    Servir contenido estático

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    // Contenido estatico
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
