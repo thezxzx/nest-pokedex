097138ad09911add9d74c252a6fe1dd56fc06d26
thezxzx <themasterzxzx@gmail.com>
2023-03-11 19:15:44 -0700

    Crear modelo-schema Pokemon e importarlo en el modulo de pokemon

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,22 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+// Para que sea un documento extiende de Document
+@Schema() // Esquema de base de datos
+export class Pokemon extends Document {
+  // id: string // Mongo me lo da
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  no: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { PokemonService } from './pokemon.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 import { PokemonController } from './pokemon.controller';
+import { PokemonService } from './pokemon.service';
 
 @Module({
   controllers: [PokemonController],
-  providers: [PokemonService]
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name, // Entity
+        schema: PokemonSchema, // Schema de la entity
+      },
+    ]),
+  ],
 })
 export class PokemonModule {}
