8811c489018eab5667f336df0e6fb2a2523a03f5
thezxzx <themasterzxzx@gmail.com>
2023-03-15 15:28:40 -0700

    Eliminar pokemon

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -85,8 +85,11 @@ export class PokemonService {
     }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove(id: string) {
+    // const pokemon = await this.findOne(id);
+    // await pokemon.deleteOne();
+    // this.pokemonModel.findByIdAndDelete();
+    return { id };
   }
 
   private handleExceptions(error: any) {
