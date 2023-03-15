f5b3bfa8f47834ecf306b7f4fb1ebbd1fa55a336
thezxzx <themasterzxzx@gmail.com>
2023-03-14 17:35:20 -0700

    Recibir la petición post del pokemon

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v2'); // Aplicar prefijo a todos los endpoints
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
