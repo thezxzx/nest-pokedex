72976bdf2e2ca110edaf7b7f5f9fae0cd36e01c0
thezxzx <themasterzxzx@gmail.com>
2023-03-21 14:12:57 -0700

    Configurar variables de entorno en el proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    // Configurar variables de entorno
+    ConfigModule.forRoot(),
     // Contenido estatico
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'), // Referencia a la base de datos
-    PokemonModule, CommonModule, SeedModule,
+    MongooseModule.forRoot(process.env.MONGODB), // Referencia a la base de datos
+    PokemonModule,
+    CommonModule,
+    SeedModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+  }
+}
